1df2bf4816353f3d6b86b4b1c363dff81efe4829
NukoOoOoOoO <nukoooo@nukooo.cc>
2023-01-11 01:19:44 +0800

    api7

--- GoodMemory/GameFunctions.cs
+++ GoodMemory/GameFunctions.cs
@@ -1,78 +1,81 @@
-﻿using Lumina.Excel.GeneratedSheets;
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
+using Lumina.Excel.GeneratedSheets;
 
-namespace GoodMemory {
-    public class GameFunctions {
-        private Plugin Plugin { get; }
+namespace GoodMemory;
 
-        private delegate byte HasItemActionUnlockedDelegate(IntPtr mem);
+public class GameFunctions
+{
+    private readonly IntPtr _cardStaticAddr;
 
-        private readonly HasItemActionUnlockedDelegate _hasItemActionUnlocked;
+    private readonly HasCardDelegate _hasCard;
 
-        private delegate byte HasCardDelegate(IntPtr localPlayer, ushort cardId);
+    private readonly HasItemActionUnlockedDelegate _hasItemActionUnlocked;
 
-        private readonly HasCardDelegate _hasCard;
+    private readonly ItemToUlongDelegate _itemToUlong;
 
-        private readonly IntPtr _cardStaticAddr;
+    public GameFunctions()
+    {
+        var hasIaUnlockedPtr = Service.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 75 A6 32 C0");
+        var hasCardPtr = Service.SigScanner.ScanText("40 53 48 83 EC 20 48 8B D9 66 85 D2 74");
+        var itemToUlongPtr = Service.SigScanner.ScanText("E8 ?? ?? ?? ?? 48 85 C0 74 33 83 7F 04 00");
 
-        public GameFunctions(Plugin plugin) {
-            this.Plugin = plugin;
+        _cardStaticAddr = Service.SigScanner.GetStaticAddressFromSig("41 0F B7 17 48 8D 0D");
 
-            var hasIaUnlockedPtr = this.Plugin.SigScanner.ScanText("E8 ?? ?? ?? ?? 84 C0 75 A9");
-            var hasCardPtr = this.Plugin.SigScanner.ScanText("40 53 48 83 EC 20 48 8B D9 66 85 D2 74");
-            this._cardStaticAddr = this.Plugin.SigScanner.GetStaticAddressFromSig("41 0F B7 17 48 8D 0D");
+        if (hasIaUnlockedPtr == IntPtr.Zero || hasCardPtr == IntPtr.Zero || _cardStaticAddr == IntPtr.Zero || itemToUlongPtr == IntPtr.Zero)
+            throw new ApplicationException("Could not get pointers for game functions");
 
-            if (hasIaUnlockedPtr == IntPtr.Zero || hasCardPtr == IntPtr.Zero || this._cardStaticAddr == IntPtr.Zero) {
-                throw new ApplicationException("Could not get pointers for game functions");
-            }
-
-            this._hasItemActionUnlocked = Marshal.GetDelegateForFunctionPointer<HasItemActionUnlockedDelegate>(hasIaUnlockedPtr);
-            this._hasCard = Marshal.GetDelegateForFunctionPointer<HasCardDelegate>(hasCardPtr);
-        }
-
-        public bool HasAcquired(Item item) {
-            var action = item.ItemAction.Value;
-
-            if (action == null) {
-                return false;
-            }
-
-            var type = (ActionType) action.Type;
-
-            if (type != ActionType.Cards) {
-                return this.HasItemActionUnlocked(item);
-            }
-
-            var cardId = item.AdditionalData;
-            var card = this.Plugin.DataManager.GetExcelSheet<TripleTriadCard>()!.GetRow(cardId);
-            return card != null && this.HasCard((ushort) card.RowId);
-        }
-
-        private unsafe bool HasItemActionUnlocked(Item item) {
-            var itemAction = item.ItemAction.Value;
-            if (itemAction == null) {
-                return false;
-            }
-
-            var type = (ActionType) itemAction.Type;
-
-            var mem = Marshal.AllocHGlobal(256);
-            *(uint*) (mem + 142) = itemAction.RowId;
-            
-            if (type == ActionType.OrchestrionRolls) {
-                *(uint*) (mem + 112) = item.AdditionalData;
-            }
-
-            var ret = this._hasItemActionUnlocked(mem) == 1;
-
-            Marshal.FreeHGlobal(mem);
-
-            return ret;
-        }
-
-        private bool HasCard(ushort cardId) {
-            return this._hasCard(this._cardStaticAddr, cardId) == 1;
-        }
+        _hasItemActionUnlocked = Marshal.GetDelegateForFunctionPointer<HasItemActionUnlockedDelegate>(hasIaUnlockedPtr);
+        _hasCard = Marshal.GetDelegateForFunctionPointer<HasCardDelegate>(hasCardPtr);
+        _itemToUlong = Marshal.GetDelegateForFunctionPointer<ItemToUlongDelegate>(itemToUlongPtr);
     }
+
+    public bool HasAcquired(Item item)
+    {
+        var action = item.ItemAction.Value;
+
+        if (action == null) return false;
+
+        var type = (ActionType)action.Type;
+
+        if (type != ActionType.Cards) return HasItemActionUnlocked(item);
+
+        var cardId = item.AdditionalData;
+        var card = Service.DataManager.GetExcelSheet<TripleTriadCard>()!.GetRow(cardId);
+        return card != null && HasCard((ushort)card.RowId);
+    }
+
+    private unsafe bool HasItemActionUnlocked(Item item)
+    {
+        var itemAction = item.ItemAction.Value;
+        if (itemAction == null) return false;
+
+        var result = _itemToUlong(item.RowId);
+        if (result == 0)
+            return false;
+
+        var type = (ActionType)itemAction.Type;
+
+        var mem = Marshal.AllocHGlobal(256);
+        *(uint*)(mem + 142) = itemAction.RowId;
+
+        if (type == ActionType.OrchestrionRolls) *(ushort*)(mem + 112) = (ushort)item.AdditionalData;
+
+        var ret = _hasItemActionUnlocked(result, 0, (long*)mem) == 1;
+
+        Marshal.FreeHGlobal(mem);
+
+        return ret;
+    }
+
+    private bool HasCard(ushort cardId)
+    {
+        return _hasCard(_cardStaticAddr, cardId) == 1;
+    }
+
+    private delegate long ItemToUlongDelegate(uint a1);
+
+    private unsafe delegate byte HasItemActionUnlockedDelegate(long mem, long a2, long* a3);
+
+    private delegate byte HasCardDelegate(IntPtr localPlayer, ushort cardId);
 }
---
+++ GoodMemory/Service.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Data;
+using Dalamud.Game;
+using Dalamud.Game.ClientState;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+
+namespace GoodMemory;
+
+internal class Service
+{
+    [PluginService] internal static ClientState ClientState { get; set; } = null!;
+
+    [PluginService] internal static DataManager DataManager { get; set; } = null!;
+
+    [PluginService] internal static SigScanner SigScanner { get; set; } = null!;
+
+    [PluginService] internal static GameGui GameUi { get; set; } = null!;
+}
