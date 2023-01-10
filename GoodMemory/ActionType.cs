0709589862ad8dadbd3d204f5afec875b405973e
NukoOoOoOoO <nukoooo@nukooo.cc>
2023-01-11 01:19:44 +0800

    api7

--- GoodMemory/ActionType.cs
+++ GoodMemory/ActionType.cs
@@ -1,33 +1,35 @@
-﻿using Lumina.Excel.GeneratedSheets;
-using System;
+﻿using System;
 using System.Linq;
+using Lumina.Excel.GeneratedSheets;
 
-namespace GoodMemory {
-    public static class ActionTypeExt {
-        private static readonly ActionType[] Valid = (ActionType[]) Enum.GetValues(typeof(ActionType));
+namespace GoodMemory;
 
-        public static bool IsValidAction(ItemAction? action) {
-            if (action == null || action.RowId == 0) {
-                return false;
-            }
+public static class ActionTypeExt
+{
+    private static readonly ActionType[] Valid = (ActionType[])Enum.GetValues(typeof(ActionType));
 
-            var type = (ActionType) action.Type;
-            return Valid.Contains(type);
-        }
+    public static bool IsValidAction(ItemAction? action)
+    {
+        if (action == null || action.RowId == 0) return false;
+
+        var type = (ActionType)action.Type;
+        return Valid.Contains(type);
     }
+}
 
-    public enum ActionType : ushort {
-        Minions = 853, // minions
-        Bardings = 1_013, // bardings
-        Mounts = 1_322, // mounts
-        CrafterBooks = 2_136, // crafter books
-        Miscellaneous = 2_633, // riding maps, blu totems, emotes/dances, hairstyles
-        Cards = 3_357, // cards
-        GathererBooks = 4_107, // gatherer books
-        OrchestrionRolls = 25_183, // orchestrion rolls
-        // these appear to be server-side
-        // FieldNotes = 19_743, // bozjan field notes
-        FashionAccessories = 20_086, // fashion accessories
-        // missing: 2_894 (always false)
-    }
+public enum ActionType : ushort
+{
+    Minions = 853, // minions
+    Bardings = 1_013, // bardings
+    Mounts = 1_322, // mounts
+    CrafterBooks = 2_136, // crafter books
+    Miscellaneous = 2_633, // riding maps, blu totems, emotes/dances, hairstyles
+    Cards = 3_357, // cards
+    GathererBooks = 4_107, // gatherer books
+    OrchestrionRolls = 25_183, // orchestrion rolls
+
+    // these appear to be server-side
+    // FieldNotes = 19_743, // bozjan field notes
+    FashionAccessories = 20_086 // fashion accessories
+    // missing: 2_894 (always false)
 }
--- GoodMemory/Plugin.cs
+++ GoodMemory/Plugin.cs
@@ -1,155 +1,177 @@
-﻿using Dalamud.Plugin;
-using Lumina.Excel.GeneratedSheets;
+﻿using System;
 using System.Linq;
-using Dalamud.Data;
-using Dalamud.Game;
-using Dalamud.Game.ClientState;
+using Dalamud;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
+using Dalamud.Hooking;
 using Dalamud.IoC;
-using XivCommon;
-using XivCommon.Functions.Tooltips;
+using Dalamud.Logging;
+using Dalamud.Plugin;
+using Lumina.Excel.GeneratedSheets;
 
-namespace GoodMemory {
-    // ReSharper disable once ClassNeverInstantiated.Global
-    public class Plugin : IDalamudPlugin {
-        public string Name => "Good Memory";
+namespace GoodMemory;
 
-        [PluginService]
-        private ClientState ClientState { get; init; } = null!;
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Plugin : IDalamudPlugin
+{
+    private readonly Hook<TooltipDelegate> tooltipHook;
+    private ItemTooltip? _tooltip;
+    private readonly GameFunctions Functions;
 
-        [PluginService]
-        internal DataManager DataManager { get; init; } = null!;
+    public Plugin(
+        [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface)
+    {
+        pluginInterface.Create<Service>();
+        Functions = new GameFunctions();
 
-        [PluginService]
-        internal SigScanner SigScanner { get; init; } = null!;
-
-        private GameFunctions Functions { get; }
-        private XivCommonBase Common { get; }
-
-        public Plugin() {
-            this.Common = new XivCommonBase(Hooks.Tooltips);
-            this.Common.Functions.Tooltips.OnItemTooltip += this.OnItemTooltip;
-
-            this.Functions = new GameFunctions(this);
-        }
-
-        public void Dispose() {
-            this.Common.Functions.Tooltips.OnItemTooltip -= this.OnItemTooltip;
-            this.Common.Dispose();
-        }
-
-        private void OnItemTooltip(ItemTooltip tooltip, ulong itemId) {
-            if (!tooltip.Fields.HasFlag(ItemTooltipFields.Description)) {
-                return;
-            }
-
-            if (itemId > 2_000_000) {
-                return;
-            }
-            
-            if (itemId > 1_000_000) {
-                itemId -= 1_000_000;
-            }
-
-            var item = this.DataManager.GetExcelSheet<Item>()!.GetRow((uint) itemId);
-            if (item == null) {
-                return;
-            }
-
-            var description = tooltip[ItemTooltipString.Description];
-
-            // Faded Copies
-            if (item.FilterGroup == 12 && item.ItemUICategory.Value?.RowId == 94 && item.LevelItem.Value?.RowId == 1) {
-                var recipeResults = this.DataManager.GetExcelSheet<Recipe>()!
-                    .Where(recipe => recipe.UnkData5.Any(ritem => ritem.ItemIngredient == item.RowId))
-                    .Select(recipe => recipe.ItemResult.Value)
-                    .Where(result => result != null)
-                    .ToArray();
-
-                foreach (var result in recipeResults) {
-                    var resultAction = result?.ItemAction?.Value;
-                    if (result == null || (ActionType?) resultAction?.Type != ActionType.OrchestrionRolls) {
-                        continue;
-                    }
-
-                    var orchId = result.AdditionalData;
-                    var orch = this.DataManager.GetExcelSheet<Orchestrion>()!.GetRow(orchId);
-                    if (orch == null) {
-                        continue;
-                    }
-
-                    this.AppendIfAcquired(description, result!, orch.Name);
-                }
-            } else {
-                var action = item.ItemAction?.Value;
-
-                if (!ActionTypeExt.IsValidAction(action)) {
-                    return;
-                }
-
-                // generate our replacement text
-                this.AppendIfAcquired(description, item);
-            }
-
-            tooltip[ItemTooltipString.Description] = description;
-        }
-
-        private void AppendIfAcquired(SeString txt, Item item, string? name = null) {
-            string yes;
-            string no;
-            string acquired;
-            string colon;
-            string parenL;
-            string parenR;
-            switch (this.ClientState.ClientLanguage) {
-                default:
-                    acquired = "Acquired";
-                    colon = ": ";
-                    yes = "Yes";
-                    no = "No";
-                    parenL = " (";
-                    parenR = ")";
-                    break;
-                case Dalamud.ClientLanguage.ChineseSimplified:
-                    acquired = "已获得";
-                    colon = "：";
-                    yes = "是";
-                    no = "否";
-                    parenL = " (";
-                    parenR = ")";
-                    break;
-                case Dalamud.ClientLanguage.French:
-                    acquired = "Acquis";
-                    colon = " : ";
-                    yes = "Oui";
-                    no = "Non";
-                    parenL = " (";
-                    parenR = ")";
-                    break;
-                case Dalamud.ClientLanguage.German:
-                    acquired = "Erhalten";
-                    colon = ": ";
-                    yes = "Ja";
-                    no = "Nein";
-                    parenL = " (";
-                    parenR = ")";
-                    break;
-                case Dalamud.ClientLanguage.Japanese:
-                    acquired = "取得";
-                    colon = "：";
-                    yes = "あり";
-                    no = "なし";
-                    parenL = "（";
-                    parenR = "）";
-                    break;
-            }
-
-            var has = this.Functions.HasAcquired(item) ? yes : no;
-            var text = name == null
-                ? $"\n{acquired}{colon}{has}"
-                : $"\n{acquired}{parenL}{name}{parenR}{colon}{has}";
-            txt.Payloads.Add(new TextPayload(text));
+        unsafe
+        {
+            var tooltipAddress = Service.SigScanner.ScanText("48 89 5C 24 ?? 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 50 48 8B 42 ??");
+            tooltipHook = Hook<TooltipDelegate>.FromAddress(tooltipAddress, TooltipDetour);
+            tooltipHook.Enable();
         }
     }
+
+    public string Name => "Good Memory";
+
+    public void Dispose()
+    {
+        tooltipHook.Dispose();
+        _tooltip?.Dispose();
+    }
+
+    private unsafe IntPtr TooltipDetour(IntPtr a1, uint** a2, byte*** a3)
+    {
+        try
+        {
+            _tooltip ??= new ItemTooltip();
+            _tooltip.SetPointer(a3);
+            OnItemTooltip(_tooltip, Service.GameUi.HoveredItem);
+        }
+        catch (Exception ex)
+        {
+            PluginLog.LogError(ex, "Failed to handle tooltip detour");
+        }
+
+        return tooltipHook.Original(a1, a2, a3);
+    }
+
+    private void OnItemTooltip(ItemTooltip tooltip, ulong itemId)
+    {
+        switch (itemId)
+        {
+            case > 2_000_000:
+                return;
+            case > 1_000_000:
+                itemId -= 1_000_000;
+                break;
+        }
+
+        var item = Service.DataManager.GetExcelSheet<Item>()!.GetRow((uint)itemId);
+        if (item == null) return;
+
+        var description = tooltip[ItemTooltip.TooltipField.ItemDescription];
+
+        foreach (var payload in description.Payloads)
+        {
+            if (payload is not TextPayload p) continue;
+            if (p.Text == null) continue;
+
+            p.Text = p.Text.Replace("\n未学习或收录", "").Replace("\n已学习或收录", "");
+        }
+
+        // Faded Copies
+        if (item.FilterGroup == 12 && item.ItemUICategory.Value?.RowId == 94 && item.LevelItem.Value?.RowId == 1)
+        {
+            var recipeResults = Service.DataManager.GetExcelSheet<Recipe>()!
+                .Where(recipe => recipe.UnkData5.Any(ritem => ritem.ItemIngredient == item.RowId))
+                .Select(recipe => recipe.ItemResult.Value)
+                .Where(result => result != null)
+                .ToArray();
+
+            foreach (var result in recipeResults)
+            {
+                var resultAction = result?.ItemAction?.Value;
+                if (result == null || (ActionType?)resultAction?.Type != ActionType.OrchestrionRolls) continue;
+
+                var orchId = result.AdditionalData;
+                var orch = Service.DataManager.GetExcelSheet<Orchestrion>()!.GetRow(orchId);
+                if (orch == null) continue;
+
+                AppendIfAcquired(description, result!, orch.Name);
+            }
+        }
+        else
+        {
+            var action = item.ItemAction?.Value;
+
+            if (!ActionTypeExt.IsValidAction(action)) return;
+
+            // generate our replacement text
+            AppendIfAcquired(description, item);
+        }
+
+        tooltip[ItemTooltip.TooltipField.ItemDescription] = description;
+    }
+
+    private void AppendIfAcquired(SeString txt, Item item, string? name = null)
+    {
+        string yes;
+        string no;
+        string acquired;
+        string colon;
+        string parenL;
+        string parenR;
+        switch (Service.ClientState.ClientLanguage)
+        {
+            default:
+                acquired = "Acquired";
+                colon = ": ";
+                yes = "Yes";
+                no = "No";
+                parenL = " (";
+                parenR = ")";
+                break;
+            case ClientLanguage.ChineseSimplified:
+                acquired = "已获得";
+                colon = "：";
+                yes = "是";
+                no = "否";
+                parenL = " (";
+                parenR = ")";
+                break;
+            case ClientLanguage.French:
+                acquired = "Acquis";
+                colon = " : ";
+                yes = "Oui";
+                no = "Non";
+                parenL = " (";
+                parenR = ")";
+                break;
+            case ClientLanguage.German:
+                acquired = "Erhalten";
+                colon = ": ";
+                yes = "Ja";
+                no = "Nein";
+                parenL = " (";
+                parenR = ")";
+                break;
+            case ClientLanguage.Japanese:
+                acquired = "取得";
+                colon = "：";
+                yes = "あり";
+                no = "なし";
+                parenL = "（";
+                parenR = "）";
+                break;
+        }
+
+        var has = Functions.HasAcquired(item) ? yes : no;
+        var text = name == null
+            ? $"\n{acquired}{colon}{has}"
+            : $"\n{acquired}{parenL}{name}{parenR}{colon}{has}";
+        txt.Payloads.Add(new TextPayload(text));
+    }
+
+    private unsafe delegate IntPtr TooltipDelegate(IntPtr a1, uint** a2, byte*** a3);
 }
